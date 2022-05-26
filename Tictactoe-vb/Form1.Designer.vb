02971d4b5561552a45c7c496e6f380d08dd4e8ee
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-05-26 20:41:14 +0700

    improved the design, working buttons for xox

---
+++ Tictactoe-vb/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.A1 = New System.Windows.Forms.Button()
+        Me.A2 = New System.Windows.Forms.Button()
+        Me.A3 = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.C1 = New System.Windows.Forms.Button()
+        Me.C2 = New System.Windows.Forms.Button()
+        Me.C3 = New System.Windows.Forms.Button()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnAbout = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblPlayer1 = New System.Windows.Forms.Label()
+        Me.lblPlayer2 = New System.Windows.Forms.Label()
+        Me.lblDraw = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'A1
+        '
+        Me.A1.BackColor = System.Drawing.Color.DarkCyan
+        Me.A1.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.A1.Location = New System.Drawing.Point(28, 23)
+        Me.A1.Name = "A1"
+        Me.A1.Size = New System.Drawing.Size(135, 99)
+        Me.A1.TabIndex = 0
+        Me.A1.UseVisualStyleBackColor = False
+        '
+        'A2
+        '
+        Me.A2.BackColor = System.Drawing.Color.DarkCyan
+        Me.A2.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.A2.Location = New System.Drawing.Point(169, 23)
+        Me.A2.Name = "A2"
+        Me.A2.Size = New System.Drawing.Size(135, 99)
+        Me.A2.TabIndex = 0
+        Me.A2.UseVisualStyleBackColor = False
+        '
+        'A3
+        '
+        Me.A3.BackColor = System.Drawing.Color.DarkCyan
+        Me.A3.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.A3.Location = New System.Drawing.Point(310, 23)
+        Me.A3.Name = "A3"
+        Me.A3.Size = New System.Drawing.Size(135, 99)
+        Me.A3.TabIndex = 0
+        Me.A3.UseVisualStyleBackColor = False
+        '
+        'B1
+        '
+        Me.B1.BackColor = System.Drawing.Color.DarkCyan
+        Me.B1.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.B1.Location = New System.Drawing.Point(28, 128)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(135, 99)
+        Me.B1.TabIndex = 0
+        Me.B1.UseVisualStyleBackColor = False
+        '
+        'B2
+        '
+        Me.B2.BackColor = System.Drawing.Color.DarkCyan
+        Me.B2.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.B2.Location = New System.Drawing.Point(169, 128)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(135, 99)
+        Me.B2.TabIndex = 0
+        Me.B2.UseVisualStyleBackColor = False
+        '
+        'B3
+        '
+        Me.B3.BackColor = System.Drawing.Color.DarkCyan
+        Me.B3.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.B3.Location = New System.Drawing.Point(310, 128)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(135, 99)
+        Me.B3.TabIndex = 0
+        Me.B3.UseVisualStyleBackColor = False
+        '
+        'C1
+        '
+        Me.C1.BackColor = System.Drawing.Color.DarkCyan
+        Me.C1.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.C1.Location = New System.Drawing.Point(28, 233)
+        Me.C1.Name = "C1"
+        Me.C1.Size = New System.Drawing.Size(135, 99)
+        Me.C1.TabIndex = 0
+        Me.C1.UseVisualStyleBackColor = False
+        '
+        'C2
+        '
+        Me.C2.BackColor = System.Drawing.Color.DarkCyan
+        Me.C2.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.C2.Location = New System.Drawing.Point(169, 233)
+        Me.C2.Name = "C2"
+        Me.C2.Size = New System.Drawing.Size(135, 99)
+        Me.C2.TabIndex = 0
+        Me.C2.UseVisualStyleBackColor = False
+        '
+        'C3
+        '
+        Me.C3.BackColor = System.Drawing.Color.DarkCyan
+        Me.C3.Font = New System.Drawing.Font("Showcard Gothic", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.C3.Location = New System.Drawing.Point(310, 233)
+        Me.C3.Name = "C3"
+        Me.C3.Size = New System.Drawing.Size(135, 99)
+        Me.C3.TabIndex = 0
+        Me.C3.UseVisualStyleBackColor = False
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.Font = New System.Drawing.Font("Showcard Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnNewGame.Location = New System.Drawing.Point(40, 375)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(112, 40)
+        Me.btnNewGame.TabIndex = 1
+        Me.btnNewGame.Text = "New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Showcard Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnReset.Location = New System.Drawing.Point(181, 375)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(112, 40)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnAbout
+        '
+        Me.btnAbout.Font = New System.Drawing.Font("Showcard Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnAbout.Location = New System.Drawing.Point(319, 375)
+        Me.btnAbout.Name = "btnAbout"
+        Me.btnAbout.Size = New System.Drawing.Size(112, 40)
+        Me.btnAbout.TabIndex = 1
+        Me.btnAbout.Text = "About"
+        Me.btnAbout.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Showcard Gothic", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.Location = New System.Drawing.Point(522, 375)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 40)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblPlayer1
+        '
+        Me.lblPlayer1.AutoSize = True
+        Me.lblPlayer1.Font = New System.Drawing.Font("Showcard Gothic", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblPlayer1.Location = New System.Drawing.Point(481, 49)
+        Me.lblPlayer1.Name = "lblPlayer1"
+        Me.lblPlayer1.Size = New System.Drawing.Size(122, 29)
+        Me.lblPlayer1.TabIndex = 2
+        Me.lblPlayer1.Text = "Player 1:"
+        '
+        'lblPlayer2
+        '
+        Me.lblPlayer2.AutoSize = True
+        Me.lblPlayer2.Font = New System.Drawing.Font("Showcard Gothic", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblPlayer2.Location = New System.Drawing.Point(481, 101)
+        Me.lblPlayer2.Name = "lblPlayer2"
+        Me.lblPlayer2.Size = New System.Drawing.Size(123, 29)
+        Me.lblPlayer2.TabIndex = 2
+        Me.lblPlayer2.Text = "Player 2:"
+        '
+        'lblDraw
+        '
+        Me.lblDraw.AutoSize = True
+        Me.lblDraw.Font = New System.Drawing.Font("Showcard Gothic", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblDraw.Location = New System.Drawing.Point(481, 159)
+        Me.lblDraw.Name = "lblDraw"
+        Me.lblDraw.Size = New System.Drawing.Size(84, 29)
+        Me.lblDraw.TabIndex = 2
+        Me.lblDraw.Text = "Draw"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.ClientSize = New System.Drawing.Size(681, 474)
+        Me.Controls.Add(Me.lblDraw)
+        Me.Controls.Add(Me.lblPlayer2)
+        Me.Controls.Add(Me.lblPlayer1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnAbout)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.C3)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.A3)
+        Me.Controls.Add(Me.C2)
+        Me.Controls.Add(Me.C1)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.A2)
+        Me.Controls.Add(Me.A1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "TicTacToe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents A1 As Button
+    Friend WithEvents A2 As Button
+    Friend WithEvents A3 As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents C1 As Button
+    Friend WithEvents C2 As Button
+    Friend WithEvents C3 As Button
+    Friend WithEvents btnNewGame As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnAbout As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblPlayer1 As Label
+    Friend WithEvents lblPlayer2 As Label
+    Friend WithEvents lblDraw As Label
+End Class
---
+++ Tictactoe-vb/Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+
+    Public player As Integer = 2
+    Public turns As Integer = 0
+    Public P1 As Integer = 0
+    Public P2 As Integer = 0
+    Public D As Integer = 0
+    Private Sub buttonClick(ByVal sender As Object, ByVal e As EventArgs) Handles C3.Click, C2.Click, C1.Click, B3.Click, B2.Click, B1.Click, A3.Click, A2.Click, A1.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Text = "" Then
+
+            If player Mod 2 = 0 Then
+                button.Text = "X"
+                player += 1
+                turns += 1
+            Else
+                button.Text = "0"
+                player += 1
+                turns += 1
+            End If
+            If CheckDraw() = True Then
+                MessageBox.Show("Draw")
+                D += 1
+                NewGame()
+            End If
+            If Winner() = True Then
+                If button.Text Is "X" Then
+                    MessageBox.Show("Player 1 Wins!")
+                    P1 += 1
+                    NewGame()
+                Else
+                    MessageBox.Show("Player 2 Wins")
+                    P2 += 1
+                    NewGame()
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub NewGame()
+        player = 2
+        turns = 0
+        lblPlayer1.Text = "Player 1 : " & P1
+        lblPlayer2.Text = "Player 2 : " & P2
+        lblDraw.Text = "Draw : " & D
+        A1.Text = ""
+        A2.Text = ""
+        A3.Text = ""
+        B1.Text = ""
+        B2.Text = ""
+        B3.Text = ""
+        C1.Text = ""
+        C2.Text = ""
+        C3.Text = ""
+    End Sub
+
+    Private Function CheckDraw() As Boolean
+        If (turns = 9) AndAlso Winner() = False Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Function Winner() As Boolean
+        If (A1.Text = A2.Text) AndAlso (A2.Text = A3.Text) AndAlso A1.Text <> "" Then
+            Return True
+        ElseIf (B1.Text = B2.Text) AndAlso (B2.Text = B3.Text) AndAlso B1.Text <> "" Then
+            Return True
+        ElseIf (C1.Text = C2.Text) AndAlso (C2.Text = C3.Text) AndAlso C1.Text <> "" Then
+            Return True
+        End If
+
+        If (A1.Text = B1.Text) AndAlso (B1.Text = C1.Text) AndAlso A1.Text <> "" Then
+            Return True
+        ElseIf (A2.Text = B2.Text) AndAlso (B2.Text = C2.Text) AndAlso A2.Text <> "" Then
+            Return True
+        ElseIf (A3.Text = B3.Text) AndAlso (B3.Text = C3.Text) AndAlso A3.Text <> "" Then
+            Return True
+        End If
+
+        If (A1.Text = B2.Text) AndAlso (B2.Text = C3.Text) AndAlso A1.Text <> "" Then
+            Return True
+        ElseIf (A3.Text = B2.Text) AndAlso (B2.Text = C1.Text) AndAlso A3.Text <> "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+
+        A1.Text = ""
+        A2.Text = ""
+        A3.Text = ""
+        B1.Text = ""
+        B2.Text = ""
+        B3.Text = ""
+        C1.Text = ""
+        C2.Text = ""
+        C3.Text = ""
+
+        lblPlayer1.Text = "Player 1: 0"
+        lblPlayer2.Text = "Player 2: 0"
+        lblDraw.Text = "Draw: 0"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim tExit As DialogResult
+
+        tExit = MessageBox.Show("Do you want to exit the game?", "Tic-Tac-Toe", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If tExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        MessageBox.Show("By Bonifacio M. De Robles Jr.", "Tic-Tac-Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
